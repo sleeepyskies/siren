85a43262afe7601e2f47378b801c657c51056fd0
skysw <skwa00001@stud.uni-saarland.de>
2025-10-02 19:36:50 +0200

    refactor Mateial to match modern standards better, cpp still empty however

--- core/src/geometry/Material.hpp
+++ core/src/geometry/Material.hpp
@@ -1,46 +1,35 @@
 #pragma once
 
-#include "renderer/Shaders.hpp"
 #include "renderer/Texture.hpp"
 #include "utilities/spch.hpp"
 
-namespace core
+namespace core::geometry
 {
 
-class Material
-{
-public:
-    explicit Material(const std::string& name) : m_name(name)
+struct Material {
+    explicit Material(const std::string& name) : name(name)
     {
     }
+    Material()  = delete;
     ~Material() = default;
 
-    void bind(const renderer::Shader& shaders) const;
+    std::string name{};
 
-    void addAmbientMap(ref<renderer::Texture2D> texture);
-    void addDiffuseMap(ref<renderer::Texture2D> texture);
-    void addSpecularMap(ref<renderer::Texture2D> texture);
-    void addRoughnessMap(ref<renderer::Texture2D> texture);
-    void addAlphaMap(ref<renderer::Texture2D> texture);
-    void addAmbience(glm::vec3 ambience);
-    void addDiffuse(glm::vec3 diffuse);
-    void addSpecular(glm::vec3 specular);
-    void addRoughness(float roughness);
-    void addAlpha(float alpha);
+    Ref<renderer::Texture2D> baseColorMap = nullptr;
+    glm::vec4 baseColorFactor{ 1 };
 
-private:
-    std::string m_name{};
+    Ref<renderer::Texture2D> metallicRoughnessMap = nullptr;
+    float metallicFactor{ 1 };
+    float roughnessFactor{ 1 };
 
-    ref<renderer::Texture2D> m_ambientMap   = nullptr;
-    ref<renderer::Texture2D> m_diffuseMap   = nullptr;
-    ref<renderer::Texture2D> m_specularMap  = nullptr;
-    ref<renderer::Texture2D> m_roughnessMap = nullptr;
-    ref<renderer::Texture2D> m_alphaMap     = nullptr;
-    std::optional<glm::vec3> m_ambience     = std::nullopt;
-    std::optional<glm::vec3> m_diffuse      = std::nullopt;
-    std::optional<glm::vec3> m_specular     = std::nullopt;
-    std::optional<float> m_roughness        = std::nullopt;
-    std::optional<float> m_alpha            = std::nullopt;
+    Ref<renderer::Texture2D> emissionMap = nullptr;
+    glm::vec3 emissionColor{ 0 };
+
+    Ref<renderer::Texture2D> occlusionMap = nullptr;
+    float oclussionStength{ 1 };
+
+    Ref<renderer::Texture2D> normalsMap = nullptr;
+    float normalScale{ 1 };
 };
 
-} // namespace core
+} // namespace core::geometry
