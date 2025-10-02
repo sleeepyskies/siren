71ad792d1bf0053d75437f0ade652229309872be
skysw <skwa00001@stud.uni-saarland.de>
2025-10-03 00:10:30 +0200

    add TextureImporter

--- core/src/assets/TextureImporter.cpp
+++ core/src/assets/TextureImporter.cpp
@@ -1,5 +1,32 @@
 #include "TextureImporter.hpp"
 
+#include <stb_image.h>
+
 namespace core::assets::TextureImporter
 {
+
+Ref<renderer::Texture2D> importTexture2D(const fs::path& path)
+{
+    if (!exists(path)) {
+        wrn("File does not exist at {}", path.string());
+        return nullptr;
+    }
+
+    stbi_set_flip_vertically_on_load(true);
+    int w, h, c;
+    const Byte* data = stbi_load(absolute(path).string().data(), &w, &h, &c, STBI_default);
+
+    if (!data) {
+        wrn("Could not load image at {}", path.string());
+        return nullptr;
+    }
+
+    // just take over default values
+    renderer::Image2DSampler sampler{};
+    Ref<renderer::Texture2D> texture =
+        makeRef<renderer::Texture2D>(data, path.filename(), sampler, w, h);
+
+    return texture;
+}
+
 } // namespace core::assets::TextureImporter
--- core/src/assets/TextureImporter.hpp
+++ core/src/assets/TextureImporter.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include "renderer/Texture.hpp"
+#include "utilities/spch.hpp"
+
 namespace core::assets::TextureImporter
 {
+
+inline Ref<renderer::Texture2D> importTexture2D(const fs::path& path);
+
 } // namespace core::assets::TextureImporter
