2872dd0957be7609ab6ed38e676187a60582704b
skysw <skwa00001@stud.uni-saarland.de>
2025-10-03 15:48:26 +0200

    shader importer, makes use of custom json like file format .sshg

--- core/src/assets/ShaderImporter.cpp
+++ core/src/assets/ShaderImporter.cpp
@@ -1,5 +1,75 @@
 #include "ShaderImporter.hpp"
 
+#include "json.hpp"
+#include <fstream>
+
 namespace core::assets::ShaderImporter
 {
+
+std::string loadFile(const fs::path& path)
+{
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        err("Could not open file {}", path);
+        return {};
+    }
+
+    std::stringstream ss;
+    ss << file.rdbuf();
+    file.close();
+
+    nfo("Loaded file {}", path);
+    return ss.str();
+}
+
+Ref<renderer::Shader> importShader(const fs::path& path)
+{
+    if (!exists(path)) {
+        wrn("File does not exist at {}", path.string());
+        return nullptr;
+    }
+
+    if (path.extension().string() != ".sshg") {
+        wrn("Invalid shader group file format at {}", path.string());
+        return nullptr;
+    }
+
+    std::ifstream file(path.string());
+    if (!file || !file.is_open()) {
+        wrn("Failed to open file at {}", path.string());
+        return nullptr;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    const std::string jsonString = buffer.str();
+    auto data = nlohmann::json::parse(jsonString.begin(), jsonString.end(), nullptr, true);
+
+    // currently, sshg only has name, and a vertex and frag shader, which are all required
+    if (!data.contains("name") || !data.contains("stages")) {
+        wrn("Invalid sshg syntax in file at {}", path.string());
+        return nullptr;
+    }
+
+    const auto& stages = data["stages"];
+
+    if (!data.contains("vertex") || !data.contains("fragment")) {
+        wrn("Invalid sshg syntax in file at {}", path.string());
+        return nullptr;
+    }
+
+    fs::path vertexPath   = (path.parent_path()) / stages["vertex"].get<std::string>();
+    fs::path fragmentPath = (path.parent_path()) / stages["fragment"].get<std::string>();
+
+    if (!exists(vertexPath) || !exists(fragmentPath)) {
+        wrn("sshg file names invalid shader files at {}", path.string());
+        return nullptr;
+    }
+
+    std::string vertexString   = loadFile(vertexPath);
+    std::string fragmentString = loadFile(fragmentPath);
+    std::string name           = data["name"];
+
+    Ref<renderer::Shader> shader = makeRef<renderer::Shader>(name, vertexString, fragmentString);
+}
 } // namespace core::assets::ShaderImporter
--- core/src/assets/ShaderImporter.hpp
+++ core/src/assets/ShaderImporter.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "renderer/Shader.hpp"
+#include "utilities/spch.hpp"
+
 namespace core::assets::ShaderImporter
 {
+
+inline Ref<renderer::Shader> importShader(const fs::path& path);
 
 } // namespace core::assets::ShaderImporter
