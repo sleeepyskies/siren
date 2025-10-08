455513779fedc8c4ff37e76536ad9cda98a6a5ba
skysw <skwa00001@stud.uni-saarland.de>
2025-10-08 18:24:07 +0200

    Add new methods to System onReady() and onShutDown()

---
+++ core/src/ecs/core/System.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace siren::ecs
+{
+
+class Scene;
+
+/**
+ * @brief The interface that all systems must implement. Allows for the data held in Component
+ * objects to be changed dynamically during the runtime of the engine.
+ */
+class System
+{
+public:
+    virtual ~System() = default;
+
+    /// @brief Handles updating this systems logic.
+    virtual void onUpdate(float delta, Scene& scene){};
+
+    /// @brief Is called once as soon as the system becomes active
+    virtual void onReady(Scene& scene){};
+
+    /// @brief Is called once just before the scene becomes inactive
+    virtual void onShutdown(Scene& scene){};
+};
+
+} // namespace siren::ecs
