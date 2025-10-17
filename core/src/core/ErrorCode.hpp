6e4dc0dc95ebf00ad0e29adfed85e354cdb4d147
skysw <skwa00001@stud.uni-saarland.de>
2025-10-17 11:43:15 +0200

    add ErrorCode enum for error handling in siren core

---
+++ core/src/core/ErrorCode.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace siren::core
+{
+
+/**
+ * @brief Enum listing various errors that can happen in siren.
+ */
+enum class ErrorCode {
+    NONE = 0,
+    // TODO: add some more lol, maybe replace for functions returning bool/void?
+    UNKNOWN,
+};
+
+} // namespace siren::core
