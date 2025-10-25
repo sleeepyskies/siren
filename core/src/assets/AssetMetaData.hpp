cfe5f05fbef12e4656484716296623e283c9c907
skysw <skwa00001@stud.uni-saarland.de>
2025-10-25 10:26:06 +0200

    add helpers to assetmetadata

--- core/src/assets/AssetMetaData.hpp
+++ core/src/assets/AssetMetaData.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "assets/Asset.hpp"
+#include "geometry/PrimitiveMesh.hpp"
 #include "utilities/spch.hpp"
 
 namespace siren::core
@@ -11,23 +12,31 @@ namespace siren::core
 
 // todo: should we introduce parent/child tracking? Would be nice for recursive hot reloading...
 
+using SourceData = std::variant<PrimitiveParams, Path>; // todo: some other source for sub-import?
+
 /**
  * @brief Struct holding an Asset's Metadata.
  */
 struct AssetMetaData {
     /// @brief Identifies the creation type of an Asset.
     enum class CreationType {
-        IMPORTED,
+        IMPORTED, // SourceData must contain Path
         SUB_IMPORT,
-        PROCEDURAL,
+        PROCEDURAL, // SourceData must contain PrimitiveParams
     };
 
     /// @brief The type of the asset
     AssetType type;
-    /// @brief The source Path of the asset. Is not required.
-    Maybe<Path> source;
+    /// @brief Information on how to re-construct this asset.
+    SourceData sourceData;
     /// @brief The Asset's creation type.
     CreationType creationType;
+
+    /// @brief Helper to get the path. Unsafe and caller must ensure the variant holds a @ref Path.
+    Path getPath() const { return std::get<Path>(sourceData); }
+    /// @brief Helper to get the path. Unsafe and caller must ensure the variant holds a @ref
+    /// PrimitiveParams.
+    PrimitiveParams getPrimitiveParams() const { return std::get<PrimitiveParams>(sourceData); }
 };
 
 } // namespace siren::core
